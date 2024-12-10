22a7cabfafd19d1883ca66418823e4016e314d4b
KornelijusKat <katinaskornelijus@gmail.com>
2024-12-10 21:01:05 +0200

    Made a Card component out of Cards, did the flip styling, added root id into index, removed previous cards

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,28 +1,15 @@
 import { useGlobalContext } from "../../context/Context";
-
+import Card from "../card/Card";
 const Cards = () =>{
-    // state tai bus is local storage data, galima turbut, kad rasytusi tik tips, bet tada manau reik reduceryje apdoroti
-    // reduceri flipstate reikia flipState padaryti, 
     const {tips, flipStates, handleFlip} = useGlobalContext();  
-
         return(
-            <section className="cards">
+            <section className="row cards g-3">
                 {tips.map((card, index)=>{
                     const {day,tip} = card
                     return(
-                        <div key={day} className={`card ${flipStates[index] ? "flip" : "" }`}>
-                            <div className="front" onClick={()=> handleFlip(index)}>                       
-                                <div className="cardNumber">
-                                    <p>{day}</p>
-                                </div>
-                                <div className="cardIcon">
-                                    <span>icon</span>
-                                </div>
-                            </div>
-                            <div className="back" onClick={()=> handleFlip(index)}>
-                                <p>{tip}</p>
-                            </div>
-                        </div>                      
+                        <div key={day} className="col-4 col-md-2">
+                            <Card day={day} tip={tip} flipStates={flipStates} index={index} handleFlip={handleFlip}></Card>
+                        </div>
                     )
                 })}
             </section>
