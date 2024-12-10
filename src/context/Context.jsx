0c666de1801e6bc72fb3c83a7feb37ad61744410
AdrijusC <asciurinskas@gmail.com>
2024-12-10 19:56:06 +0200

    Paupdatinta kad butu local sotrage
    Turetu veikt

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,39 +1,48 @@
+import React, { createContext, useReducer, useContext, useEffect } from "react";
 import reducer from "../reducers/reducer";
-import { useReducer } from "react";
-import { useContext } from "react";
 import { HANDLE_FLIP, WARNING } from "../actions/actions";
-import React from "react";
-import data from "../data/tips.json"
+import data from "../data/tips.json";
+
+// Uzloadina tipsus jei yra data
+const storedTips = JSON.parse(localStorage.getItem("tips"));
+const storedFlipStates = JSON.parse(localStorage.getItem("flipStates"));
 
 const initialState = {
-    tips: data.tips,
-    flipStates: data.tips.map(()=>false)
-}
+  tips: storedTips || data.tips,
+  flipStates: storedFlipStates || data.tips.map(() => false),
+};
 
-const AppContext = React.createContext(null)
+const AppContext = createContext(null);
 
-const AppProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState)
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const handleFlip = (index)=>{
-        console.log(state)
-        const today = new Date();
-        const currentDay = today.getDate()
-        if (index <= currentDay){
-            dispatch({type:"HANDLE_FLIP", payload: index});
-        } else {
-            dispatch({type:"WARNING"})
-        }
+  const handleFlip = (index) => {
+    const today = new Date();
+    const currentDay = today.getDate();
+
+    if (index <= currentDay) {
+      dispatch({ type: HANDLE_FLIP, payload: index });
+    } else {
+      dispatch({ type: WARNING });
     }
-    return (
-        <AppContext.Provider value={{...state, handleFlip}}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  };
 
-export const useGlobalContext = ()=>{
-    return useContext(AppContext)
-}
+  //Issaugo tipsus i local storage
+  useEffect(() => {
+    localStorage.setItem("tips", JSON.stringify(state.tips));
+    localStorage.setItem("flipStates", JSON.stringify(state.flipStates));
+  }, [state.tips, state.flipStates]);
 
-export {AppContext, AppProvider}
+  return (
+    <AppContext.Provider value={{ ...state, handleFlip }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,25 +1,25 @@
 import { HANDLE_FLIP, WARNING } from "../actions/actions";
 
-const reducer = (state, action)=>{
-    switch (action.type){
-        case 'HANDLE_FLIP':
-            return {
-                ...state, 
-                flipStates: state.flipStates.map((flipState, index)=>{
-                    if ( index == action.payload){
-                        return true
-                    } else {
-                        return flipState
-                    }
-                }) 
-            }  
-           
-        case 'WARNING':
-            alert('You cannot view this advent day yet')
-            return {...state}
-        default:
-            throw new Error(`no matching "${action.type}" action type`)
+const reducer = (state, action) => {
+  switch (action.type) {
+    case HANDLE_FLIP: {
+      const updatedFlipStates = state.flipStates.map((flipState, index) =>
+        index === action.payload ? true : flipState
+      );
+
+      return {
+        ...state,
+        flipStates: updatedFlipStates,
+      };
     }
-}
+
+    case WARNING:
+      alert("You cannot view this advent day yet");
+      return state;
+
+    default:
+      throw new Error(`No matching "${action.type}" action type`);
+  }
+};
 
 export default reducer;
