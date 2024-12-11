93c3ea1a642fc16e67ba92479b876d50261fba43
Sefleris <134661117+Shefleris@users.noreply.github.com>
2024-12-11 21:34:44 +0200

    Merge: Update light/dark theme switcher (#12)
    Update theme mode switcher

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -2,14 +2,14 @@ import { useGlobalContext } from "../../context/Context";
 import getIcon from "../../helpers/getIcon.jsx";
 import Card from "../card/Card";
 const Cards = () =>{
-    const {tips, flipStates, handleFlip} = useGlobalContext();  
+    const {tips, flipStates, themeState, handleFlip} = useGlobalContext();  
         return(
             <section className="row cards g-3">
                 {tips.map((card, index)=>{
-                    const {day,tip} = card
+                    const {day, tip} = card
                     return(
                         <div key={day} className="col-6 col-md-4 col-lg-3 col-xl-2">
-                            <Card day={day} tip={tip} flipStates={flipStates} index={index} handleFlip={handleFlip} icon={getIcon(day)}></Card>
+                            <Card day={day} tip={tip} flipStates={flipStates} themeState={themeState} index={index} handleFlip={handleFlip} icon={getIcon(day)}></Card>
                         </div>
                     )
                 })}
