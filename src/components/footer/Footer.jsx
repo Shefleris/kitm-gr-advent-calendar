5490b2b8252fa35e96aeb7b85b9bf370a43bc927
Mantas Sinkevicius <sinkevicius.man@gmail.com>
2024-12-11 21:34:26 +0200

    Added footer logo and responsiveness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,26 @@
 const Footer = () => {
-    return (
-        <footer className="footer text-center py-3 d-flex justify-content-evenly align-items-center">
-            <p className="mb-0">© 2024 Advantage Travel</p>
-            <a href="#" className="link"><i className="fa-solid fa-computer"></i>www.advantagemembers.com</a>
-            <div className="social-icons d-flex gap-3">
-                <a href="#" className="me-2"><i className="fa-brands fa-facebook me-1"></i> Facebook</a>
-                <a href="#" className="me-2"><i className="fa-brands fa-instagram me-1"></i> Instagram</a>
-                <a href="#" className="me-2"><i className="fa-brands fa-twitter me-1"></i> Twitter</a>
-                <a href="#" className="me-2"><i className="fa-brands fa-linkedin me-1"></i> Linkedin</a>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer py-3 d-flex flex-column flex-lg-row justify-content-evenly align-items-center">
+        <img src="/src/img/advantage_travel.png" alt="Logo" className="footer-logo" />
+        <a href="#" className="link">
+          <i className="fa-solid fa-computer"></i>www.advantagemembers.com
+        </a>
+        <div className="social-icons d-flex gap-3">
+          <a href="#" className="me-2">
+            <i className="fa-brands fa-facebook me-1"></i> Facebook
+          </a>
+          <a href="#" className="me-2">
+            <i className="fa-brands fa-instagram me-1"></i> Instagram
+          </a>
+          <a href="#" className="me-2">
+            <i className="fa-brands fa-twitter me-1"></i> Twitter
+          </a>
+          <a href="#" className="me-2">
+            <i className="fa-brands fa-linkedin me-1"></i> Linkedin
+          </a>
+        </div>
+    </footer>
+  );
+};
 
 export default Footer;
