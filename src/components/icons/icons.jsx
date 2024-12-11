12213db90c414d51294f224c1168dd4d01b67a3c
Mantas Sinkevicius <sinkevicius.man@gmail.com>
2024-12-11 18:22:27 +0200

    Added icons and icons array

---
+++ src/components/icons/icons.jsx
@@ -0,0 +1,29 @@
+import treeIcon from "../../img/1tree.svg";
+import reindeerIcon from "../../img/2reindeer.svg";
+import santaIcon from "../../img/3santa.svg";
+import sledIcon from "../../img/4christmas-sled-sleigh.svg";
+import snowGlobeIcon from "../../img/5christmas-snow-globe.svg";
+import surpriseIcon from "../../img/6christmas-shock-surprise.svg";
+import fireplaceIcon from "../../img/7christmas-fireplace-warm.svg";
+import angelWingIcon from "../../img/8christmas-angel-wing.svg";
+import candleIcon from "../../img/9christmas-candle-winter.svg";
+import starBookmarkIcon from "../../img/10christmas-star-bookmark.svg";
+import hutIcon from "../../img/11christmas-hut-building.svg";
+import santaClausIcon from "../../img/12christmas-santa-claus.svg";
+
+const Icons = [
+  treeIcon,
+  reindeerIcon,
+  santaIcon,
+  sledIcon,
+  snowGlobeIcon,
+  surpriseIcon,
+  fireplaceIcon,
+  angelWingIcon,
+  candleIcon,
+  starBookmarkIcon,
+  hutIcon,
+  santaClausIcon,
+];
+
+export default Icons;
